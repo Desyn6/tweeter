59a3c83d6464371535f253887e3d57bdb1e04ce3
Denny <Dennyng1989@hotmail.com>
2023-03-19 20:24:53 -0700

    Implement remaining character counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+  // --- our code goes here ---
+  const charLimit = 140;
+
+  $("#tweet-text").on("input", function() {
+    // calc charleft from length of string @ tweet-text
+    const charLeft = charLimit - $(this).val().length;
+
+    // Up to parent, down to 'div' > 'output'
+    const charCount = $(this).parent().children('div').children('output');
+
+    // write remaining char count to counter
+    charCount.val(charLeft);
+
+    // set/remove color class for counter
+    (charLeft < 0 ? charCount.addClass("red") : charCount.removeClass("red"));
+  })
+});
+
