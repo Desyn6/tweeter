2f6c621f25e372060b324db567ccf33653144e56
Denny <Dennyng1989@hotmail.com>
2023-03-22 19:31:07 -0700

    Implement renderTweet function

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,50 +4,83 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-$(document).ready(function() {
-
-  const tweetData = {
+/* Temporary hard-coded tweet data for accessing dynamically*/
+const data = [
+  {
     "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-        "handle": "@SirIsaac"
-      },
+      "name": "Rick",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@theAstley"
+    },
     "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
+      "text": "Never gonna give you up."
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Ricky",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@theAster" },
+    "content": {
+      "text": "Never gonna let you down."
+    },
+    "created_at": 1461113959088
+  },
+  {
+    "user": {
+      "name": "Richtly",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@Asterley"
+    },
+    "content": {
+      "text": "Never gonna run around and desert you."
+    },
     "created_at": 1461116232227
   }
+];
 
-  /** This function will generate a Tweet article element
-   *  using a standard tweet-data object.
-   * 
-   * @param {*} tweet 
-   * @returns 
-   */
-  const createTweetElement = (tweet) => {
-    const $tweet = $(`
-    <article class="tweet">
-      <header>
-        <img src="${tweet.user.avatars}" alt="User icon">
-        <div>${tweet.user.name}</div>
-        <div class="handle">${tweet.user.handle}</div>
-      </header>
-      <p class="text">${tweet.content.text}</p>
-      <footer>
-        <div class="date"> ${tweet.created_at}</div>
-        <i class="fa-solid fa-flag"></i>
-        <i class="fa-solid fa-retweet"></i>
-        <i class="fa-solid fa-heart"></i>
-      </footer>
-    </article>`);
+/** This function will generate a tweet article element
+ *  using a standard tweet-data object.
+ *
+ * @param {*} "tweet-data object"
+ * @returns "tweet article element"
+ */
+const createTweetElement = (tweet) => {
+  const $tweet = $(`
+  <article class="tweet">
+    <header>
+      <img src="${tweet.user.avatars}" alt="User icon">
+      <div>${tweet.user.name}</div>
+      <div class="handle">${tweet.user.handle}</div>
+    </header>
+    <p class="text">${tweet.content.text}</p>
+    <footer>
+      <div class="date"> ${tweet.created_at}</div>
+      <i class="fa-solid fa-flag"></i>
+      <i class="fa-solid fa-retweet"></i>
+      <i class="fa-solid fa-heart"></i>
+    </footer>
+  </article>`);
 
-    return $tweet;
+  return $tweet;
+};
+
+/** This function accepts an array of tweet-data objects,
+ *  calls createTweetElement on each tweet-data object,
+ *  then appends the tweet article to the index.html container.
+ *
+ * @param {*} "array of tweet-data objects"
+ */
+const renderTweets = (tweets) => {
+  for (const tweet of tweets) {
+    $('.tweets-container').append(createTweetElement(tweet));
   }
+};
 
-  // Test to see if a tweet article is returned
-  // Arbitrarily mapped to the navbar right double-down icon.
-  $('.fa-angles-down').on('click', ()=> {
-    console.log('Tweet Article:', createTweetElement(tweetData));
-    console.log('HTML Text:', createTweetElement(tweetData).html())
-  })
+/* Call renderTweets on tweet data object*/
+$(document).ready(function() {
+  renderTweets(data);
 });
